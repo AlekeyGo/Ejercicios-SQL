efb588ae367b536d6b3dcf3850f71aad5dd97ea5
AlekeyGo <danielvillota44@gmail.com>
2026-02-27 00:47:10 -0500

    8 ejercicios faltantes

---
+++ 27.sql
@@ -0,0 +1,1 @@
+WITH ventas_mensuales AS (SELECT strftime('%m', fecha_venta) AS mes, SUM(total_venta) AS total_mes FROM ventas WHERE strftime('%Y', fecha_venta) = '2025' GROUP BY mes) SELECT MAX(total_mes) - MIN(total_mes) AS diferencia_ventas FROM ventas_mensuales;
---
+++ 28.sql
@@ -0,0 +1,1 @@
+SELECT clientes.nombre, MAX(ventas.fecha_venta) AS ultima_compra FROM clientes INNER JOIN ventas ON clientes.id_cliente = ventas.id_cliente GROUP BY clientes.nombre HAVING MAX(ventas.fecha_venta) < DATE('now', '-6 months')
