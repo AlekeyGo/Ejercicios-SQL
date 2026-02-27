5d8c34fa4193dd06b8551fd5b8b9b594e3bb707f
AlekeyGo <danielvillota44@gmail.com>
2026-02-27 00:33:42 -0500

    5 ejercicios mas (18, 19, 20, 21, 22)

---
+++ 18.sql
@@ -0,0 +1,1 @@
+SELECT clientes.nombre, (strftime('%Y','now') - strftime('%Y',clientes.fecha_nacimiento)) AS edad_aproximada, SUM(ventas.total_venta) AS total_gastado FROM clientes INNER JOIN ventas ON clientes.id_cliente = ventas.id_cliente GROUP BY clientes.id_cliente, clientes.nombre HAVING edad_aproximada > 60
---
+++ 21.sql
@@ -0,0 +1,1 @@
+SELECT clientes.nombre, COUNT(ventas.id_venta) AS veces_usado FROM ventas INNER JOIN clientes ON clientes.id_cliente = ventas.id_cliente WHERE ventas.cupon_usado = 'ULTIMO_SUSPIRO' GROUP BY clientes.nombre HAVING COUNT(ventas.id_venta) >= 3
